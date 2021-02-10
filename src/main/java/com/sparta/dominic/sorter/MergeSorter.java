3846e81387e3375eb271a07106dabb59ff8a38ee
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-02-10 10:20:06 +0000

    Refactor code

--- src/main/java/com/sparta/dominic/sorter/MergeSorter.java
+++ src/main/java/com/sparta/dominic/sorter/MergeSorter.java
@@ -44,26 +44,17 @@ public class MergeSorter implements Sorter
 			sortAuxiliary(arrayToSort, (midPoint + 1), endIndex);
 
 			// create left and right temp arrays and merge them.
-			createAndMergeSubArrays(arrayToSort, startIndex, midPoint, endIndex);
+			int[] leftArray = new int[midPoint - startIndex + 1];
+			int[] rightArray = new int[endIndex - midPoint];
+
+			// Copy the subarrays to temp arrays.
+			ArrayUtil.copyArray(arrayToSort, startIndex, leftArray, 0);
+			ArrayUtil.copyArray(arrayToSort, (midPoint + 1), rightArray, 0);
+
+			// Merge subarrays.
+			int[] mergedArray = ArrayUtil.mergeSortedArrays(leftArray, rightArray);
+			ArrayUtil.copyArray(mergedArray, 0, arrayToSort, startIndex, mergedArray.length);
 		}
 		return arrayToSort;
-	}
-
-	/*
-	 * Creates sub arrays using the given index and merges them.
-	 */
-	private static void  createAndMergeSubArrays(int[] arrayToSort, int startIndex, int midPoint, int endIndex)
-	{
-		int[] leftArray = new int[midPoint - startIndex + 1];
-		int[] rightArray = new int[endIndex - midPoint];
-
-		// Copy the subarrays to temp arrays.
-		ArrayUtil.copyArray(arrayToSort, startIndex, leftArray, 0);
-		ArrayUtil.copyArray(arrayToSort, (midPoint + 1), rightArray, 0);
-
-		// merge the temp arrays.
-		int[] merged = ArrayUtil.mergeSortedArrays(leftArray, rightArray);
-		// copy the merged array back to the original array.
-		ArrayUtil.copyArray(merged, 0, arrayToSort, startIndex, merged.length);
 	}
 }
