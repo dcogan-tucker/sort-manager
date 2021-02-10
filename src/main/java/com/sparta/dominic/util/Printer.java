1188ff20ca0f10800c3afc7b459c901ec2bb27ae
Dominic Cogan-Tucker <dom.cogantucker@gmail.com>
2021-02-10 09:46:40 +0000

    Move Printer to util package

---
+++ src/main/java/com/sparta/dominic/util/Printer.java
@@ -0,0 +1,25 @@
+package com.sparta.dominic.util;
+
+public class Printer
+{
+	/**
+	 * Print the given message to the console.
+	 *
+	 * @param message The message to print.
+	 */
+	public static void printMessage(String message)
+	{
+		System.out.println(message);
+	}
+
+	/**
+	 * Print a formatted message to the console.
+	 *
+	 * @param formatString The format string.
+	 * @param args The arguments referenced by the format string.
+	 */
+	public static void printFormattedMessage(String formatString, Object... args)
+	{
+		System.out.printf(formatString + "\n", args);
+	}
+}
